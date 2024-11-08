356d2ff92de97515b718afe135565abe72d964f8
Mark J Chen <mjchen.gene@gmail.com>
2024-11-08 10:25:24 -0500

    feat: test GenomicRange1BasedTranscriptFeatureAnnotator with TERT

--- tests/annotators/rangeannotators/genomicrange1basedtranscriptfeatureannotator/test_tert.py
+++ tests/annotators/rangeannotators/genomicrange1basedtranscriptfeatureannotator/test_tert.py
@@ -11,14 +11,14 @@ from transcriptfeatures.transcriptfeaturescreators import (
 )
 
 
-class GenomicRange1BasedTranscriptFeatureAnnotatorErbb2TestCase(unittest.TestCase):
-    """Test GenomicRange1BasedTranscriptFeatureAnnotator class with ERBB2."""
+class GenomicRange1BasedTranscriptFeatureAnnotatorTertTestCase(unittest.TestCase):
+    """Test GenomicRange1BasedTranscriptFeatureAnnotator class with TERT."""
 
     @classmethod
     def setUpClass(cls) -> None:
         super().setUpClass()
-        tx_ac = "NM_004448.2"
-        genome_ac = "NC_000017.10"
+        tx_ac = "NM_198253.2"
+        genome_ac = "NC_000005.9"
         creator = TranscriptFeaturesCreator(
             tx_ac=tx_ac,
             genome_ac=genome_ac,
@@ -29,53 +29,53 @@ class GenomicRange1BasedTranscriptFeatureAnnotatorErbb2TestCase(unittest.TestCas
         cls.genome_ac = genome_ac
 
     def test_exon(self):
-        """Exon example: exon 19."""
+        """Exon example: exon 4."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37880218, end=37880219
+            ac=self.genome_ac, start=1280416, end=1280417
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
             transcript_features=self.transcript_features,
         ).annotate()
-        self.assertEqual(feature_annotation.format(), "exon 19")
+        self.assertEqual(feature_annotation.format(), "exon 4")
 
     def test_intron(self):
-        """Intron example: intron 12."""
+        """Intron example: intron 4."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37856590, end=37856591
+            ac=self.genome_ac, start=1280225, end=1280226
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
             transcript_features=self.transcript_features,
         ).annotate()
-        self.assertEqual(feature_annotation.format(), "intron 1")
+        self.assertEqual(feature_annotation.format(), "intron 4")
 
     def test_exon_intron_boundary(self):
-        """Exon-intron boundary: exon 8 3' and intron 8 5'."""
+        """Exon-intron boundary: exon 6 3' and intron 6 5'."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37868300, end=37868301
+            ac=self.genome_ac, start=1278755, end=1278756
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
             transcript_features=self.transcript_features,
         ).annotate()
-        self.assertEqual(feature_annotation.format(), "exon 8 - intron 8")
+        self.assertEqual(feature_annotation.format(), "exon 6 - intron 6")
 
     def test_intron_exon_boundary(self):
-        """Intron-exon boundary: intron 19 3' and exon 20 5'."""
+        """Intron-exon boundary: intron 6 3' and exon 7 5'."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37880978, end=37880979
+            ac=self.genome_ac, start=1272395, end=1272396
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
             transcript_features=self.transcript_features,
         ).annotate()
-        self.assertEqual(feature_annotation.format(), "intron 19 - exon 20")
+        self.assertEqual(feature_annotation.format(), "intron 6 - exon 7")
 
     def test_promoter(self):
         """Promoter region."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37855595, end=37855595
+            ac=self.genome_ac, start=1296496, end=1296496
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
@@ -86,7 +86,7 @@ class GenomicRange1BasedTranscriptFeatureAnnotatorErbb2TestCase(unittest.TestCas
     def test_out_5_prime(self):
         """Out of gene body: 5 prime."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37853595, end=37855595
+            ac=self.genome_ac, start=1296496, end=1298496
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
@@ -97,10 +97,10 @@ class GenomicRange1BasedTranscriptFeatureAnnotatorErbb2TestCase(unittest.TestCas
     def test_out_3_prime(self):
         """Out of gene body: 3 prime."""
         genomic_range_1_based = GenomicRange1Based(
-            ac=self.genome_ac, start=37880979, end=37884997
+            ac=self.genome_ac, start=1245813, end=1272395
         )
         feature_annotation = GenomicRange1BasedTranscriptFeatureAnnotator(
             genomic_range_1_based=genomic_range_1_based,
             transcript_features=self.transcript_features,
         ).annotate()
-        self.assertEqual(feature_annotation.format(), "exon 20 - ?")
+        self.assertEqual(feature_annotation.format(), "exon 7 - ?")
